325211b2da8d894d227840ffdaa84f0ddb36b91e
Pwnpalm <blockabears@gmail.com>
2022-04-01 15:05:30 -0400

    added contract address

--- backend/src/config.js
+++ backend/src/config.js
@@ -89,7 +89,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xD4d3AF06ccE8e057518e53CA0d9d669D72663292"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
