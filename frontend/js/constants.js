9fb2109430f39f5501f9f46333359bea05783a70
Pwnpalm <blockabears@gmail.com>
2022-04-01 15:20:41 -0400

    contract ddress front end

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xD4d3AF06ccE8e057518e53CA0d9d669D72663292"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
