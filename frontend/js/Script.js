18f8afa97eab4309e1ec732ccc24f3cb47a2a54c
Pwnpalm <blockabears@gmail.com>
2022-04-03 00:54:23 -0400

    cardscript

--- frontend/js/Script.js
+++ frontend/js/Script.js
@@ -1,36 +1,3 @@
-(function($) {
-    $.fn.tilt = function() {
-        var perspective = '500px',
-            delta = 20,
-            width = this.width(),
-            height = this.height(),
-            midWidth = width / 2,
-            midHeight = height / 2;
-        this.on({
-            mousemove: function(e) {
-                var pos = $(this).offset(),
-                    cursPosX = e.pageX - pos.left,
-                    cursPosY = e.pageY - pos.top,
-                    cursCenterX = midWidth - cursPosX,
-                    cursCenterY = midHeight - cursPosY;
-
-                $(this).css('transform', 'perspective(' + perspective + ') rotateX(' + (cursCenterY / delta) + 'deg) rotateY(' + -(cursCenterX / delta) + 'deg)');
-                $(this).removeClass('is-out');
-            },
-            mouseleave: function() {
-                $(this).addClass('is-out');
-            }
-        });
-        //Return
-        return this;
-    };
-}(jQuery));
-
-//Set plugin on cards
-$('.card').tilt();
-
-
-
 //Scroll reveal
 window.sr = ScrollReveal();
 sr.reveal('.reveal', { duration: 6000 });
